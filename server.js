c041fa43e4506bc7dd2ab6e196637f727ae9614c
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-13 14:32:57 -0200

    executando o express

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000');
+});
+
+app.get('/', function(req, resp) {
+    resp.send(
+        `<html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+        </html>`
+    );
+});
+
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `<html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Livros </h1>
+            </body> 
+        </html>`
+    );
+});
+
+/*
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
@@ -29,5 +63,4 @@ const servidor = http.createServer(function (req, resp) {
  	resp.end(html);
 });
 servidor.listen(3000);
-
-
+*/
