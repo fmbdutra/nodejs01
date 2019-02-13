e280ce4c8f8d78d930518f10e54e272afc7b7c31
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-13 14:50:16 -0200

    Encapsulando as rotas

--- server.js
+++ server.js
@@ -4,32 +4,6 @@ app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000');
 });
 
-app.get('/', function(req, resp) {
-    resp.send(
-        `<html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>`
-    );
-});
-
-app.get('/livros', function(req, resp) {
-    resp.send(
-        `<html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Livros </h1>
-            </body> 
-        </html>`
-    );
-});
-
 /*
 const http = require('http');
 
