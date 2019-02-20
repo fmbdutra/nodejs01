fedb754413299f99523e99064e8871f81c789ecb
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-20 10:24:37 -0300

    add busca por id (sem html)

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -41,6 +41,30 @@ class LivroDao{
             )
         })
     }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+
+
+
 }
 
 module.exports = LivroDao;
